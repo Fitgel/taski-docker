06c9c25f93fc0cbc9838fb3c9289e086e4798725
Daniil <fitgeledition@gmail.com>
2026-04-21 00:14:12 +0300

    Check after added kittygram

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,11 +8,10 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-# ALLOWED_HOSTS = []
 ALLOWED_HOSTS = [
     'localhost',
     '123.123.123.123',
-    'taski.denzelprakk.hopto.org'
+    'denzelprakk.hopto.org'
 ]
 
 
